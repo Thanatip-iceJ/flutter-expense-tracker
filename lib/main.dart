ce02949de041a66a29c71b0110cc4f4b5ba5df0c
Thanatip-iceJ <p.thanatip1@gmail.com>
2024-03-01 11:13:13 +0700

    init: initializing

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: const Text("Expense Tracker App")),
+          body: Container()),
+    );
+  }
+}
